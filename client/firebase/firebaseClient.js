bb81c8fbfa1661ebaa34fb7275c1282eed7e9b8e
mansi0829 <mansigundre1@gmail.com>
2022-05-01 08:10:07 +0530

    env sample added

---
+++ client/firebase/firebaseClient.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+
+const FIREBASE_CONFIG = {
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+};
+
+export default function firebaseClient() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(FIREBASE_CONFIG);
+  }
+}
